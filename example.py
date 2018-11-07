00332fa72516a7a452d1eb6b48a8a8aa5e8b169a
小宝 <ilsanboo@gmail.com>
2018-11-07 11:44:58 +0800

    update

--- example.py
+++ example.py
@@ -1,16 +1,70 @@
 import ipdb,sys
 
-db = ipdb.Reader("c:/work/tiantexin/bb/v6/mydata6vipday4.ipdb")
 
-print(db.support_languages())
+def test_free():
+    db = ipdb.City("c:/work/ipdb/city.free.ipdb")
+    print(db.is_ipv4(), db.is_ipv6())
+    print(db.languages())
+    print(db.fields())
+    print(db.build_time())
+    print(db.find("1.1.1.1", "CN"))
+    print(db.find("118.28.1.1", "CN"))
 
-try:
-    print(db.find("2000:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF", "CN"))
-except Exception as e:
-    print(e)
+    try:
+        print(db.find("2000:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF", "CN"))
+    except Exception as e:
+        print(e)
 
-sys.exit(0)
-print(db.find_map("2001:250:201::"))
 
-info = db.find_info("2a06:e881:3800::")
-print(info.country_name, info.region_name, info.city_name, info.owner_domain, info.isp_domain, info.latitude, info.longitude, info.timezone, info.utc_offset)
+def test_city_ipv4():
+    db = ipdb.City("c:/work/ipdb/city.ipv4.ipdb")
+    print(db.is_ipv4(), db.is_ipv6())
+    print(db.languages())
+    print(db.fields())
+    print(db.build_time())
+    print(db.find("1.1.1.1", "CN"))
+    print(db.find_map("8.8.8.8", "CN"))
+    print(db.find_info("118.28.1.1", "CN").country_name)
+
+
+def test_city_ipv6():
+    db = ipdb.City("c:/work/ipdb/city.ipv6.ipdb")
+    print(db.is_ipv4(), db.is_ipv6())
+    print(db.languages())
+    print(db.fields())
+    print(db.build_time())
+    print(db.find("2001:250:200::", "CN"))
+
+    try:
+        print(db.find("2000:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF", "CN"))
+    except Exception as e:
+        print(e)
+
+
+def test_district():
+    db = ipdb.District("c:/work/ipdb/china_district.ipdb")
+    print(db.is_ipv4(), db.is_ipv6())
+    print(db.languages())
+    print(db.fields())
+    print(db.build_time())
+    print(db.find("1.12.13.255", "CN"))
+    print(db.find_map("1.12.13.255", "CN"))
+    print(db.find_info("1.12.13.255", "CN").country_name)
+
+
+def test_base_station():
+    db = ipdb.BaseStation("c:/work/ipdb/base_station.ipdb")
+    print(db.is_ipv4(), db.is_ipv6())
+    print(db.languages())
+    print(db.fields())
+    print(db.build_time())
+    print(db.find_map("117.136.83.55", "CN"))
+
+
+def test_idc_list():
+    db = ipdb.IDC("c:/work/ipdb/idc_list.ipdb")
+    print(db.find_map("1.1.1.1", "CN"))
+    print(db.find_map("8.8.8.8", "CN"))
+
+test_city_ipv6()
+test_base_station()
