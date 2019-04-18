221b01d45f2ed49bd446f69322edf5ac77289e08
小宝 <ilsanboo@gmail.com>
2019-04-18 13:24:08 +0800

    ipip-ipdb-1.2.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='ipip-ipdb',
-    version="1.2.1",
+    version="1.2.2",
     description=(
         'IPIP.net officially supported IP database ipdb format parsing library'
     ),
